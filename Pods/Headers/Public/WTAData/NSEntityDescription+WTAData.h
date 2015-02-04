1f07505a25ae24bdf06cd9fe3ff6848c62ff1a43
Robert Thompson <rothomp3@vt.edu>
2015-02-04 10:57:39 -0500

    Commit all the things

--- Pods/Headers/Public/WTAData/NSEntityDescription+WTAData.h
+++ Pods/Headers/Public/WTAData/NSEntityDescription+WTAData.h
@@ -1,15 +1,1 @@
-//
-//  NSEntityDescription+WTAData.h
-//  WTAData
-//
-//  Copyright (c) 2014 WillowTreeApps. All rights reserved.
-//
-
-#import <CoreData/CoreData.h>
-
-@interface NSEntityDescription (WTAData)
-
-- (NSAttributeDescription *)primaryAttribute;
-- (NSString *)primaryAttributeString;
-
-@end
+../../../../../WTAData/WTAData/categories/NSEntityDescription+WTAData.h
